0a6568c39b558bce06a3548ae590f0746a243f8b
praveenachowdary-png <praveenachowdary57@gmail.com>
2021-07-24 16:19:31 +0530

    take three inputs

--- sortingarthmitic.sh
+++ sortingarthmitic.sh
@@ -1,1 +1,7 @@
-echo "welcome to sorting"
+
+#!/bin/bash -x
+echo "Welcome to sorting arthmetic computation"
+read -p "enter the value of a:" a
+read -p "enter the value of b:" b
+read -p "enter the value of c:" c
+echo "$a $b $c"
